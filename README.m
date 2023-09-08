be22f441cb8b367a7b18c44feb3ca618b1d7fb38
Ghostemmy <144439764+Ghostemmy@users.noreply.github.com>
2023-09-08 10:51:51 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi I’m @Ghostemmy
+- 👀 I’m interested in software engineering
+- 🌱 I’m currently Studying for my bachelor degree in comp.science
+- 💞️ I’m looking to collaborate on building many projects associating with my Discipline 
+- 📫 How to reach me "emmanueletty2023@gmail.com"
+
+<!---
+Ghostemmy/Ghostemmy is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
